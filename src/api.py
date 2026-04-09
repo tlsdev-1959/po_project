feb4dda34c42572d6ef77f5f79aecc4848c3e5a1
Tanner Summers <tsummers@thelexingtonschool.org>
2026-04-09 03:21:34 -0400

    Initial commit

---
+++ src/api.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Request
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/get/budget', name='get budget')
+async def getBudget(request: Request):
+    return {'budget': 0}
+
+if __name__ == '__main__':
+    uvicorn.run('api:app', host='localhost', port=9343, reload=True)
