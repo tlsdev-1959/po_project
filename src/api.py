58a33a82430270caaf9362788f99d9fc1e9296f5
Tanner Summers <tsummers@thelexingtonschool.org>
2026-04-09 04:40:19 -0400

    Update host

--- src/api.py
+++ src/api.py
@@ -8,4 +8,4 @@ async def getBudget(request: Request):
     return {'budget': 0}
 
 if __name__ == '__main__':
-    uvicorn.run('api:app', host='localhost', port=9343, reload=True)
+    uvicorn.run('api:app', host='0.0.0.0', port=9343, reload=True)
